b74a7c24d37012109cef7ae537121e3fbd28430e
Lleyton-Ariton <lleytonariton@MacBookLleyton.local>
2020-08-19 19:48:27 -0400

    updated pyramid plane stacking layer

--- firewatch/src/utils/layers.py
+++ firewatch/src/utils/layers.py
@@ -22,11 +22,39 @@ class PaddedConv2d(nn.Conv2d):
 
 class StackPyramidPlanes(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_features: int=256, bottom_channels: int=4, pyramid_depth: int=4, step: int=2):
         super().__init__()
 
+        self.in_features = in_features
+        self.bottom_channels = bottom_channels
+        self.step = step
+
+        self.layers = nn.ModuleList([])
+        sizes = []
+
+        previous = self.bottom_channels
+        for i in range(pyramid_depth):
+            if i < 1:
+                sizes.append((previous, previous))
+            else:
+                if i % 2 == 0:
+                    sizes.append((previous * 2, previous * 2))
+                    previous = sizes[i][0]
+
+                else:
+                    sizes.append((
+                        (previous * 2) - 1,
+                        (previous * 2) - 1
+                    ))
+
+                    previous = sizes[i][0]
+
+            self.layers.append(nn.Conv2d(self.in_features, self.in_features,
+                                         kernel_size=(sizes[i][0] - 2,
+                                                      sizes[i][1] - 2)))
+
     def forward(self, pyramid_planes: List[torch.Tensor]) -> torch.Tensor:
-        for i, p in enumerate(pyramid_planes):
-            pyramid_planes[i] = nn.Conv2d(256, 256, kernel_size=(p.size()[-2], p.size()[-1]))(p)
+        for i, (plane, conv_reducer) in enumerate(zip(pyramid_planes, self.layers)):
+            pyramid_planes[i] = conv_reducer(plane)
 
         return torch.stack(pyramid_planes)
